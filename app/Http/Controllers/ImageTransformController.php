21c8f1b41dcc3038b665e8d6e123c4fca0c817e2
Carlos Barboza <barbozacarlos678@gmail.com>
2025-02-23 23:48:33 -0300

    Created images store routes, created resize method on ImageTransformController.

--- app/Http/Controllers/ImageTransformController.php
+++ app/Http/Controllers/ImageTransformController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Image;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManager;
 
 class ImageTransformController extends Controller
 {
-    //
+    public function rezise(string $id, int $width, int $height): JsonResponse
+    {
+        try {
+            $image_data = Image::findOrFail($id);
+            $image = Storage::get("{$image_data->path}/{$image_data->name}");
+            $manager = ImageManager::imagick();
+
+            if (!$image) return response()->json(['message' => 'Image not found!'], 404);
+
+            $resized_image = $manager->read($image)
+                ->resize($width, $height);
+
+            return response()->json([
+                'message' => 'Resized image!',
+                'image' => $resized_image
+            ], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Image data not found!'
+            ], 404);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'An error occurred on resize image!'
+            ], 400);
+        }
+    }
+
+    public function crop() {}
+    public function rotate() {}
+    public function watermark() {}
+    public function flip() {}
+    public function mirror() {}
+    public function compress() {}
+    public function changeFormat() {}
+    public function applyFilters() {}
 }
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ImageStoreController;
+use App\Http\Controllers\ImageTransformController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::group([
-    'middleware' => 'auth:api',
-    'prefix' => 'auth'
-], function () {
+Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/images', ImageStoreController::class);
+    Route::post('/resize', [ImageTransformController::class, 'resize']);
 });
